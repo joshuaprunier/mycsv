3c7597a3052d86a963748a227d4368a4c5196242
Joshua Prunier <jprunier@netprospex.com>
2015-06-30 11:09:22 -0400

    build file

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Linux
+echo
+echo "Building Linux"
+mkdir -p bin/linux
+go build -o bin/linux/mycsv mycsv.go password_linux.go term_linux.go
+if [[ $? -eq 0 ]]; then
+	echo "	mycsv - OK"
+else
+	echo "	mycsv - FAILED"
+fi
+
+# Windows
+echo
+echo "Building Windows"
+mkdir -p bin/windows
+GOOS=windows GOARCH=amd64 go build -o bin/windows/mycsv.exe mycsv.go password_win.go
+if [[ $? -eq 0 ]]; then
+	echo "	mycsv.exe - OK"
+else
+	echo "	mycsv.exe - FAILED"
+fi
+
+# Darwin
+echo
+echo "Building Darwin"
+mkdir -p bin/darwin
+GOOS=darwin GOARCH=amd64 go build -o bin/darwin/mycsv mycsv.go password_linux.go term_darwin.go
+if [[ $? -eq 0 ]]; then
+	echo "	mycsv_mac - OK"
+else
+	echo "	mycsv_mac - FAILED"
+fi
+
+echo
+echo "Done!"
