137e3241526449db5c8b2426b6249dc348cbc93d
Joshua Prunier <jprunier@netprospex.com>
2015-07-31 14:35:29 -0400

    Added benchmarks, use with: go test -bench .

--- csv_writer_test.go
+++ csv_writer_test.go
@@ -82,3 +82,62 @@ func TestError(t *testing.T) {
 		t.Error("Error should not be nil")
 	}
 }
+
+func BenchmarkWritePlainCharacters(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		buf := &bytes.Buffer{}
+		f := NewWriter(buf)
+		f.Write([]sql.RawBytes{[]byte(`abcdef`)})
+		f.Flush()
+		err := f.Error()
+
+		if err != nil {
+			b.Errorf("Unexpected error: %s\n", err)
+		}
+	}
+}
+
+func BenchmarkWriteEscapeCharacters(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		buf := &bytes.Buffer{}
+		f := NewWriter(buf)
+		f.Write([]sql.RawBytes{[]byte(`a,b,,c\n"def"`)})
+		f.Flush()
+		err := f.Error()
+
+		if err != nil {
+			b.Errorf("Unexpected error: %s\n", err)
+		}
+	}
+}
+
+func BenchmarkWriteBaconIpsum(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		buf := &bytes.Buffer{}
+		f := NewWriter(buf)
+		f.Write([]sql.RawBytes{[]byte(`
+		Bacon ipsum dolor amet beef ribs fatback cupim, pig pancetta pork loin ribeye shankle t-bone beef strip steak capicola. Jerky alcatra rump, andouille doner turducken jowl. Turducken landjaeger beef, rump drumstick ham shoulder pork belly biltong boudin meatball jowl doner fatback. Flank ball tip pork belly brisket. Flank spare ribs tail alcatra, doner turducken sausage. Beef ribs drumstick spare ribs biltong ham hock rump jowl, ham brisket kevin prosciutto.
+
+		Beef andouille spare ribs, jowl alcatra doner bresaola chuck landjaeger pork ball tip. Sausage sirloin ham chicken bacon. Rump pastrami tenderloin pancetta brisket andouille kielbasa fatback cow ribeye. Shankle chicken leberkas, pancetta shank drumstick doner filet mignon pastrami cupim. Drumstick filet mignon tail doner, tenderloin flank shank ground round pork loin landjaeger. Bresaola fatback filet mignon flank kielbasa shoulder. Chuck cupim bacon leberkas.
+
+		Biltong brisket tail, swine chuck kevin picanha cow rump corned beef landjaeger cupim meatloaf porchetta ball tip. Kielbasa ham doner beef ribs t-bone tongue cow drumstick flank filet mignon fatback. Boudin salami ham hock, tail sausage spare ribs pancetta meatloaf flank filet mignon jowl meatball doner. Chicken salami shank, jerky meatloaf short ribs bacon cow.
+
+		Chicken ham leberkas, short loin tri-tip capicola fatback tenderloin pig sausage meatloaf tongue beef sirloin shoulder. Short loin chuck beef jowl drumstick fatback pork loin ribeye tri-tip turkey picanha kevin short ribs rump. Meatloaf turkey frankfurter flank. Salami drumstick rump, tail bacon kevin meatball jowl ribeye swine ball tip bresaola. Doner corned beef sausage flank cupim fatback. Spare ribs pork loin meatloaf picanha turducken landjaeger pastrami salami. Fatback turkey drumstick ham landjaeger bresaola tri-tip short loin.
+		`)})
+		f.Flush()
+		err := f.Error()
+
+		if err != nil {
+			b.Errorf("Unexpected error: %s\n", err)
+		}
+	}
+}
